3ffc452e2472e152cacd22082589a1538fa3ea15
a22006675 <alexis.atrokhov@etu.univ-amu.fr>
2023-06-02 19:00:22 +0200

    Test Partie 2

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -1,27 +1,11 @@
 package fr.amu.iut.cc3;
 
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
+import javafx.scene.shape.Circle;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class ToileController implements Initializable {
@@ -32,18 +16,39 @@ public class ToileController implements Initializable {
     private static int noteMaximale = 20;
 
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
+
 
     int getXRadarChart(double value, int axe ){
         return (int) (rayonCercleExterieur + Math.cos(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
     }
 
+
     int getYRadarChart(double value, int axe ){
         return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
     }
 
+    @FXML
+    public void drawPoint() {
+        Circle point = new Circle();
+        point.setCenterX(225);
+        point.setCenterY(225);
+        point.setRadius(5);
+    }
+
+    /*
+
+    public void drawPoint() {
+        Circle point = new Circle();
+        point.setCenterX(getXRadarChart(comp1, 1));
+        point.setCenterY(getYRadarChart(comp1, 1));
+        point.setRadius(5);
+    }
+
+     */
 }
